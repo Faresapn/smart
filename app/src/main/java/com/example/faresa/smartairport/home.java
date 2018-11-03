cf1026333d59154d82cc89f4d73cf0844c4b9af0
faresa <faresaprasetyon@gmail.com>
2018-11-03 20:04:29 +0700

    jsh

--- app/src/main/java/com/example/faresa/smartairport/home.java
+++ app/src/main/java/com/example/faresa/smartairport/home.java
@@ -3,6 +3,7 @@ package com.example.faresa.smartairport;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -10,12 +11,14 @@ import android.widget.ImageView;
 public class home extends AppCompatActivity implements View.OnClickListener{
 
     private ImageView imgmoveactivity;
+    private CardView crdview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        crdview = (CardView) findViewById(R.id.System);
+        crdview.setOnClickListener(this);
         imgmoveactivity = (ImageView) findViewById(R.id.profile_image);
         imgmoveactivity.setOnClickListener(this);
     }
@@ -25,6 +28,12 @@ public class home extends AppCompatActivity implements View.OnClickListener{
             case R.id.profile_image:
                 Intent moveIntent = new Intent(home.this, profile.class);
                 startActivity(moveIntent);
+                break;
+            case R.id.System:
+                Intent moveIntent1 = new Intent(home.this, Alert.class);
+                startActivity(moveIntent1);
+                break;
+            default:
                 break;
 
         }
