a815c902efd79f3c3ec6a1c9db8352f5cabf99b6
faresa <faresaprasetyon@gmail.com>
2018-11-12 20:33:40 +0700

    assd

---
+++ app/src/main/java/com/example/faresa/smartairport/bijbpay.java
@@ -0,0 +1,13 @@
+package com.example.faresa.smartairport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class bijbpay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bijbpay);
+    }
+}
